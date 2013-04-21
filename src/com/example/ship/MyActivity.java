0feec380bbe95cd6381a2d33b0012b99f20843ba
Den Medov <dengithub@gmail.com>
2013-04-21 05:47:48 +0400

    update all and add sprite

--- src/com/example/ship/MyActivity.java
+++ src/com/example/ship/MyActivity.java
@@ -6,17 +6,26 @@ import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.opengl.texture.TextureOptions;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.atlas.bitmap.BuildableBitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.source.IBitmapTextureAtlasSource;
+import org.andengine.opengl.texture.atlas.buildable.builder.BlackPawnTextureAtlasBuilder;
+import org.andengine.opengl.texture.atlas.buildable.builder.ITextureAtlasBuilder;
+import org.andengine.opengl.texture.region.TextureRegion;
 import org.andengine.ui.activity.SimpleBaseGameActivity;
 
-public class MyActivity extends SimpleBaseGameActivity
-{
+public class MyActivity extends SimpleBaseGameActivity {
     private Camera camera;
     private static final int CAMERA_WIDTH = 720;
     private static final int CAMERA_HEIGHT = 480;
+    private TextureRegion shipTextureRegion;
+    Scene scene;
 
     @Override
-    public EngineOptions onCreateEngineOptions()
-    {
+    public EngineOptions onCreateEngineOptions() {
         camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
         EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED,
                 new FillResolutionPolicy(), camera);
@@ -24,15 +33,42 @@ public class MyActivity extends SimpleBaseGameActivity
     }
 
     @Override
-    protected void onCreateResources()
-    {
-
+    protected void onCreateResources() {
+        // lol
+        // full path assets/gfx
+        BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
+        // Create the bitmap texture atlas for the sprite's texture region
+        BuildableBitmapTextureAtlas mBitmapTextureAtlas =
+                new BuildableBitmapTextureAtlas(mEngine.getTextureManager(), 1024, 1024,
+                TextureOptions.BILINEAR);
+        /* Create the sprite's texture region via
+        the BitmapTextureAtlasTextureRegionFactory */
+        shipTextureRegion = BitmapTextureAtlasTextureRegionFactory.
+                createFromAsset(mBitmapTextureAtlas, this, "ship.png");  // ship.png in assets/gfx folder
+        // Build the bitmap texture atlas
+        try {
+            mBitmapTextureAtlas.build(new BlackPawnTextureAtlasBuilder<IBitmapTextureAtlasSource
+                                                                      , BitmapTextureAtlas >(0, 1, 1));
+        } catch (ITextureAtlasBuilder.TextureAtlasBuilderException e) {
+            e.printStackTrace();
+        }
+        // Load the bitmap texture atlas into the device's gpu memory
+        mBitmapTextureAtlas.load();
     }
 
     @Override
-    protected Scene onCreateScene()
-    {
-        Scene scene = new Scene();
+    protected Scene onCreateScene() {
+        scene = new Scene();
+        final float shipX = CAMERA_WIDTH * 0.5f;
+        final float shipY = CAMERA_HEIGHT * 0.5f;
+
+        Sprite shipSprite = new Sprite(shipX
+                                        , shipY
+                                        , shipTextureRegion
+                                        , mEngine.getVertexBufferObjectManager());
+
+        scene.attachChild(shipSprite);
+
         scene.setBackground(new Background(0.09804f, 0.6274f, 0.8784f));
         return scene;
     }
