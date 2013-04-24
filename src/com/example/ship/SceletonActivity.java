c32d6ad7b2cd4ce8a7e3f4628d8f99989892bb1d
Navi91 <navi91@inbox.ru>
2013-04-24 19:45:38 +0400

    Сделано позиционирование по центру

--- src/com/example/ship/SceletonActivity.java
+++ src/com/example/ship/SceletonActivity.java
@@ -1,7 +1,7 @@
 package com.example.ship;
 
+import android.graphics.Typeface;
 import android.util.Log;
-import android.widget.Toast;
 import com.example.ship.Atlas.ResourceManager;
 import org.andengine.engine.camera.Camera;
 import org.andengine.engine.options.EngineOptions;
@@ -9,20 +9,26 @@ import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
-import org.andengine.entity.sprite.ButtonSprite;
 import org.andengine.entity.sprite.Sprite;
+import org.andengine.entity.text.Text;
 import org.andengine.input.touch.TouchEvent;
+import org.andengine.opengl.font.Font;
+import org.andengine.opengl.font.FontFactory;
+import org.andengine.opengl.texture.TextureOptions;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
 import org.andengine.opengl.texture.region.TextureRegion;
 import org.andengine.ui.activity.BaseGameActivity;
 import org.andengine.util.color.Color;
 
 public class SceletonActivity extends BaseGameActivity {
-    private static final int CAMERA_WIDTH = 720;
-    private static final int CAMERA_HEIGHT = 480;
+    private static final int CAMERA_WIDTH = 960;
+    private static final int CAMERA_HEIGHT = 600;
     private Camera camera;
     private TextureRegion shipTextureRegion;
     private Scene scene;
     private ResourceManager resMan;
+    Font mFont;
+
 
     @Override
     public EngineOptions onCreateEngineOptions() {
@@ -36,25 +42,21 @@ public class SceletonActivity extends BaseGameActivity {
     public void onCreateResources(OnCreateResourcesCallback pOnCreateResourcesCallback) {
         resMan = new ResourceManager();
         resMan.loadAllTextures(this,mEngine.getTextureManager());
+        mFont = FontFactory.create(mEngine.getFontManager(), mEngine.
+                getTextureManager(), 256, 256, Typeface.create(Typeface.DEFAULT,
+                Typeface.NORMAL), 32f, true, Color.BLACK_ABGR_PACKED_INT);
+        mFont.load();
         pOnCreateResourcesCallback.onCreateResourcesFinished();
     }
 
     @Override
     public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback) {
         scene = new Scene();
-        final float shipX = CAMERA_WIDTH * 0.5f;
-        final float shipY = CAMERA_HEIGHT * 0.5f;
 
-        // create ship sprite
-        /*Sprite shipSprite = new Sprite( shipX
-                                      , shipY
-                                      , resMan.getLoadedTextureRegion("ship")
-                                      , mEngine.getVertexBufferObjectManager()); */
-
-        Sprite shipSprite = new Sprite( CAMERA_WIDTH * 0.5f
-                                                  , CAMERA_HEIGHT * 0.5f
-                                                  , resMan.getLoadedTextureRegion("ship")
-                                                  , mEngine.getVertexBufferObjectManager()){
+        Sprite startButtonSprite = new Sprite( CAMERA_WIDTH * 0.5f - resMan.getLoadedTextureRegion("button_menu").getWidth() * 0.5f
+                                             , CAMERA_HEIGHT * 0.2f
+                                             , resMan.getLoadedTextureRegion("button_menu")
+                                             , mEngine.getVertexBufferObjectManager()){
             @Override
             public boolean onAreaTouched(TouchEvent pSceneTouchEvent,
                 float pTouchAreaLocalX, float pTouchAreaLocalY){
@@ -66,9 +68,30 @@ public class SceletonActivity extends BaseGameActivity {
             }
         };
 
-       scene.registerTouchArea(shipSprite);
-       scene.attachChild(shipSprite);
-       // scene.attachChild(startButtonSprite);
+        Sprite exitButtonSprite = new Sprite( CAMERA_WIDTH * 0.5f - resMan.getLoadedTextureRegion("button_menu").getWidth() * 0.5f
+                                            , CAMERA_HEIGHT * 0.4f
+                                            , resMan.getLoadedTextureRegion("button_menu")
+                                            , mEngine.getVertexBufferObjectManager()){
+            @Override
+            public boolean onAreaTouched(TouchEvent pSceneTouchEvent,
+                                         float pTouchAreaLocalX, float pTouchAreaLocalY){
+
+                if ( pSceneTouchEvent.isActionDown() ){
+                    Log.d("AAAAAAAAAwwwwwwwwwwwwwwwwwwAAAAAAAA:","AAAAAAAAAAwwwwwwwwwwwwwwwwAAAAAAAAAAAA");
+                }
+                return true;
+            }
+        };
+
+        Text text = new Text(startButtonSprite.getRotationCenterX(), 0.0f, mFont,"Start", mEngine.getVertexBufferObjectManager());
+
+
+        scene.registerTouchArea(startButtonSprite);
+        scene.registerTouchArea(exitButtonSprite);
+        scene.attachChild(startButtonSprite);
+        scene.attachChild(exitButtonSprite);
+        startButtonSprite.attachChild(text);
+        text.setPosition(startButtonSprite.getWidth() * 0.5f - text.getWidth() * 0.5f,startButtonSprite.getHeight() * 0.5f - text.getHeight() * 0.5f);
         Color backgroundColor = new Color(0.09804f, 0.6274f, 0.8784f);
         scene.setBackground(new Background(backgroundColor));
         scene.setTouchAreaBindingOnActionDownEnabled(true);
