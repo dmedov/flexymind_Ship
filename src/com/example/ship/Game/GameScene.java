e970beaf28214b5adb014e8e795e5202cd988ccb
Den Medov <dengithub@gmail.com>
2013-05-03 00:31:10 +0400

    fix after comments in pull request

--- src/com/example/ship/Game/GameScene.java
+++ src/com/example/ship/Game/GameScene.java
@@ -1,30 +1,27 @@
 package com.example.ship.Game;
 
-import android.graphics.Point;
 import com.example.ship.Atlas.ResourceManager;
-import com.example.ship.R;
 import com.example.ship.SceletonActivity;
 import org.andengine.engine.Engine;
 import org.andengine.entity.Entity;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
 import org.andengine.entity.sprite.Sprite;
-import org.andengine.entity.util.FPSLogger;
 import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.util.color.Color;
 
 public class GameScene extends Scene {
+    private static int layerCount = 0;
+    private static final int LAYER_BACKGROUND  = layerCount++;
+    private static final int LAYER_FIRST_WAVE  = layerCount++;
+    private static final int LAYER_SECOND_WAVE = layerCount++;
+    private static final int LAYER_THIRD_WAVE  = layerCount++;
+    private static final int LAYER_PERISCOPE   = layerCount++;
 
     private final SceletonActivity activity;
     private final Engine mEngine;
     private final ResourceManager resourceManager;
-    private static final int LAYER_COUNT = 4;
 
-    private static final int LAYER_BACKGROUND = 0;
-    private static final int LAYER_FIRST_WAVE = LAYER_BACKGROUND + 1;
-    private static final int LAYER_SECOND_WAVE = LAYER_FIRST_WAVE + 1;
-    private static final int LAYER_THIRD_WAVE = LAYER_SECOND_WAVE + 1;
-    private static final int LAYER_PERISCOPE = LAYER_THIRD_WAVE + 1;
 
     public GameScene(final SceletonActivity activity){
         super();
@@ -37,23 +34,23 @@ public class GameScene extends Scene {
 
     private void createBackground() {
 
-        for(int i = 0; i < LAYER_COUNT; i++) {
+        for(int i = 0; i < layerCount; i++) {
             this.attachChild(new Entity());
         }
 
         ITextureRegion backgroundTexture = resourceManager.getLoadedTextureRegion("gameBackground");
-        Sprite backgroundImage = new Sprite( 0
-                                           , 0
-                                           , backgroundTexture
-                                           , mEngine.getVertexBufferObjectManager());
+        Sprite backgroundSprite = new Sprite( 0
+                                            , 0
+                                            , backgroundTexture
+                                            , mEngine.getVertexBufferObjectManager());
 
-        ITextureRegion waveTexture = resourceManager.getLoadedTextureRegion("wave");
+        ITextureRegion waveSprite = resourceManager.getLoadedTextureRegion("wave");
         Sprite waveImage = new Sprite( 0
                                      , backgroundTexture.getHeight() / 3
-                                     , waveTexture
+                                     , waveSprite
                                      , mEngine.getVertexBufferObjectManager());
 
-        this.getChildByIndex(LAYER_BACKGROUND).attachChild(backgroundImage);
+        this.getChildByIndex(LAYER_BACKGROUND).attachChild(backgroundSprite);
         this.getChildByIndex(LAYER_FIRST_WAVE).attachChild(waveImage);
         Color backgroundColor = new Color(0.09804f, 0.6274f, 0.8784f);
         this.setBackground(new Background(backgroundColor));
