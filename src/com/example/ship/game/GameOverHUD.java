7cb1f05b03cdf80d5c1da3c56ba5225a17fa2117
Navi91 <navi91@inbox.ru>
2013-05-16 12:15:01 +0400

    Создан класс GameOverHUD

---
+++ src/com/example/ship/game/GameOverHUD.java
@@ -0,0 +1,88 @@
+package com.example.ship.game;
+
+import android.graphics.PointF;
+import android.graphics.Typeface;
+import com.example.ship.R;
+import com.example.ship.SceletonActivity;
+import org.andengine.engine.camera.hud.HUD;
+import org.andengine.engine.handler.timer.ITimerCallback;
+import org.andengine.engine.handler.timer.TimerHandler;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.entity.text.Text;
+import org.andengine.opengl.font.Font;
+import org.andengine.opengl.font.FontFactory;
+import org.andengine.opengl.texture.region.ITextureRegion;
+import org.andengine.util.color.Color;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Dmitriy
+ * Date: 16.05.13
+ * Time: 11:51
+ * To change this template use File | Settings | File Templates.
+ */
+public class GameOverHUD extends HUD {
+    private static final int FONT_ATLAS_SIDE = 512;
+    private static final float RELATIVE_GAME_OVER_BACKGROUND = 0.3f;
+    private static final float RELATIVE_GAME_OVER_FONT = 0.16f;
+    private final PointF cameraSize;
+    private SceletonActivity activity;
+    private Font gameOverFont;
+    private Sprite gameOverBackgound;
+
+    public GameOverHUD(SceletonActivity activity){
+        super();
+
+        this.activity = activity;
+        cameraSize = new PointF( this.activity.getCamera().getWidthRaw()
+                , this.activity.getCamera().getHeightRaw());
+
+        createGameOverBackground();
+        createGameOverFont();
+    }
+
+    private void createGameOverFont() {
+        gameOverFont = FontFactory.create( activity.getEngine().getFontManager()
+                                         , activity.getEngine().getTextureManager()
+                                         , FONT_ATLAS_SIDE
+                                         , FONT_ATLAS_SIDE
+                                         , Typeface.create(Typeface.DEFAULT, Typeface.NORMAL)
+                                         , gameOverBackgound.getHeight() * RELATIVE_GAME_OVER_FONT
+                                         , true
+                                         , Color.WHITE_ABGR_PACKED_INT);
+        gameOverFont.load();
+    }
+
+    private void createGameOverBackground() {
+        ITextureRegion gameOverTexture =
+                activity.getResourceManager().getLoadedTextureRegion(R.drawable.gameoverbackground);
+        gameOverBackgound = new Sprite( 0
+                                      , 0
+                                      , gameOverTexture
+                                      , activity.getEngine().getVertexBufferObjectManager());
+        gameOverBackgound.setPosition( cameraSize.x / 2 - gameOverBackgound.getWidth() / 2
+                                     , cameraSize.y / 2 - gameOverBackgound.getHeight() / 2);
+        gameOverBackgound.setScale(cameraSize.y * RELATIVE_GAME_OVER_BACKGROUND / gameOverBackgound.getHeight());
+        this.attachChild(gameOverBackgound);
+    }
+
+    private void createGameOverHUDLabels() {
+        Text gameOverText = new Text( 0
+                               , 0
+                               , gameOverFont
+                               , "Game Over"
+                               , activity.getEngine().getVertexBufferObjectManager());
+        gameOverText.setPosition( gameOverBackgound.getWidth() / 2 - gameOverText.getWidth() / 2
+                                , gameOverBackgound.getHeight() / 2 - gameOverText.getHeight() / 2);
+        gameOverBackgound.attachChild(gameOverText);
+
+        Text scoreText = new Text( 0
+                                 , 0
+                                 , gameOverFont
+                                 , activity.getSceneSwitcher().getGameScene().getPlayer().getStringScore()
+                                 , activity.getEngine().getVertexBufferObjectManager());
+        scoreText.setPosition( gameOverBackgound.getWidth() / 2 - scoreText.getWidth() / 2
+                             , gameOverBackgound.getHeight() / 2 - scoreText.getHeight() / 2);
+        gameOverBackgound.attachChild(scoreText);
+    }
+}
