da83671dfdcd59d2265f98e6d8580fbffd82bbf1
Den Medov <dengithub@gmail.com>
2013-05-24 23:52:55 +0400

    added CRandrom common

--- src/com/example/ship/bonus/CRandom.java
+++ src/com/example/ship/bonus/CRandom.java
@@ -1,5 +1,7 @@
 package com.example.ship.bonus;
 
+import java.util.Random;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Denis
@@ -8,4 +10,10 @@ package com.example.ship.bonus;
  * To change this template use File | Settings | File Templates.
  */
 public class CRandom {
+    static Random rnd = new Random();
+
+    public static boolean play(float propability) {
+        float randomValue = rnd.nextFloat();
+        return randomValue < propability;
+    }
 }
