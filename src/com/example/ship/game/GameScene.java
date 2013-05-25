6570bbb04ce498feefaa90881376a8802257ade7
Den Medov <dengithub@gmail.com>
2013-05-25 05:39:07 +0400

    refactor

--- src/com/example/ship/game/GameScene.java
+++ src/com/example/ship/game/GameScene.java
@@ -222,7 +222,6 @@ public class GameScene extends Scene {
             if (!blockBonus) {
                 createShipBonus(deadShip);
             }
-            //activity.getSceneSwitcher().getGameScene().getPlayer().getLevel().incrementLevelProgress();
             ships.remove(deadShip);
             Log.d("1log", "killed");
             deadShip = null;
