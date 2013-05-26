23c1cedaca156cfe0f74bfcc8e27ad0671656c3c
vasyafo <vaf257@gmail.com>
2013-05-26 22:08:06 +0400

    заготовка класса HighScoresManager

---
+++ src/com/example/ship/game/HighScoresManager.java
@@ -0,0 +1,41 @@
+package com.example.ship.game;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Vasya
+ * Date: 26.05.13
+ * Time: 21:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class HighScoresManager {
+
+    private static final String FILE_NAME = "highscores.txt";
+    private static final int MAX_SIZE = 10;
+
+    private int currentSize;
+
+    public HighScoresManager() {
+
+    }
+
+    public String getScore(int i) {
+        return null;
+    }
+
+    public void addScore(String score) {
+
+    }
+
+    public boolean testScore(int score) {
+        return true;
+    }
+
+    public void clearScores() {
+
+    }
+
+    private void save() {
+
+    }
+
+}
