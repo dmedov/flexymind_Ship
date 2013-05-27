ecb2eaa332908c0ad92246226b351a0ef5de1f42
Gregory <morgot.gregory@gmail.com>
2013-05-27 22:18:05 +0400

    добавил недостающие файлы

---
+++ src/com/example/ship/game/particlesystem/Recipe.java
@@ -0,0 +1,94 @@
+package com.example.ship.game.particlesystem;
+
+import android.graphics.Point;
+import android.graphics.PointF;
+import org.andengine.util.color.Color;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Gregory
+ * Date: 27.05.13
+ * Time: 11:28
+ * To change this template use File | Settings | File Templates.
+ */
+public class Recipe {
+    public int      typeId;
+    public int      level;
+    public float    centerX;
+    public float    centerY;
+    public float    width;
+    public float    height;
+    public Point    lifeTime;
+    public PointF   scaleRange;
+    public Color    initFireColor;
+    public Color    endFireColor;
+    public float    rateMinimum;
+    public float    rateMaximum;
+    public int      particlesMaximum;
+    public float    minVelocityY;
+    public float    maxVelocityY;
+    public float    alpha;
+
+    public Recipe( int typeId, int level, float centerX, float centerY, float width, float height, Point lifeTime
+                 , PointF scaleRange, Color initFireColor, Color endFireColor, float rateMinimum, float rateMaximum
+                 , int particlesMaximum, float minVelocityY, float maxVelocityY) {
+        this.typeId = typeId;
+        this.level = level;
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.width = width;
+        this.height = height;
+        this.lifeTime = lifeTime;
+        this.scaleRange = scaleRange;
+        this.initFireColor = initFireColor;
+        this.endFireColor = endFireColor;
+        this.rateMinimum = rateMinimum;
+        this.rateMaximum = rateMaximum;
+        this.particlesMaximum = particlesMaximum;
+        this.minVelocityY = minVelocityY;
+        this.maxVelocityY = maxVelocityY;
+        this.alpha = 1f;
+    }
+    public Recipe( int typeId, int level, float centerX, float centerY, float width, float height, Point lifeTime
+            , PointF scaleRange, Color initFireColor, Color endFireColor, float rateMinimum, float rateMaximum
+            , int particlesMaximum, float minVelocityY, float maxVelocityY, float alpha) {
+        this.typeId = typeId;
+        this.level = level;
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.width = width;
+        this.height = height;
+        this.lifeTime = lifeTime;
+        this.scaleRange = scaleRange;
+        this.initFireColor = initFireColor;
+        this.endFireColor = endFireColor;
+        this.rateMinimum = rateMinimum;
+        this.rateMaximum = rateMaximum;
+        this.particlesMaximum = particlesMaximum;
+        this.minVelocityY = minVelocityY;
+        this.maxVelocityY = maxVelocityY;
+        this.alpha = alpha;
+    }
+    public String toString() {
+        String string = new String();
+        string += "typeId: " + ((Integer) typeId).toString()+"\n";
+        string += "level: " + ((Integer) level).toString()+"\n";
+        string += "centerX: " + ((Float) centerX).toString()+"\n";
+        string += "centerY: " + ((Float) centerY).toString()+"\n";
+        string += "width: " + ((Float) width).toString()+"\n";
+        string += "height: " + ((Float) height).toString()+"\n";
+        string += "lifeTime: " + lifeTime.toString()+"\n";
+        string += "scaleRange: " + scaleRange.toString()+"\n";
+        string += "initFireColor: " + initFireColor.toString()+"\n";
+        string += "endFireColor: " + endFireColor.toString()+"\n";
+        string += "rateMinimum: " + ((Float) rateMinimum).toString()+"\n";
+        string += "rateMaximum: " + ((Float) rateMaximum).toString()+"\n";
+        string += "particlesMaximum: " + ((Integer) particlesMaximum).toString()+"\n";
+        string += "minVelocityY: " + ((Float) minVelocityY).toString()+"\n";
+        string += "maxVelocityY: " + ((Float) maxVelocityY).toString()+"\n";
+        string += "alpha: " + ((Float) alpha).toString()+"\n";
+        return string;
+    }
+
+
+}
---
+++ src/com/example/ship/game/particlesystem/Recipes.java
@@ -0,0 +1,189 @@
+package com.example.ship.game.particlesystem;
+
+
+import android.graphics.Point;
+import android.graphics.PointF;
+import com.example.ship.R;
+import org.andengine.util.color.Color;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Gregory
+ * Date: 27.05.13
+ * Time: 11:45
+ * To change this template use File | Settings | File Templates.
+ */
+public class Recipes {
+    public static Recipe missileboatLevel1 = new Recipe( R.drawable.missileboat
+                                                       , 1
+                                                       , 110f
+                                                       , 61f
+                                                       , 23
+                                                       , 0f
+                                                       , new Point(1, 1)
+                                                       , new PointF(1f, 0.2f)
+                                                       , new Color(1f, 0.3f, 0f)
+                                                       , new Color(0.7f, 0.05f, 0f)
+                                                       , 10f
+                                                       , 10f
+                                                       , 100
+                                                       , -10f
+                                                       , -20f );
+
+    public static Recipe missileboatLevel2 = new Recipe( R.drawable.missileboat
+                                                       , 2
+                                                       , 40f
+                                                       , 45f
+                                                       , 100f
+                                                       , 0f
+                                                       , new Point(1, 2)
+                                                       , new PointF(1f, 0.2f)
+                                                       , new Color(1f, 0.5f, 0f)
+                                                       , new Color(0.7f, 0.05f, 0f)
+                                                       , 30f
+                                                       , 25f
+                                                       , 100
+                                                       , -10f
+                                                       , -20f
+                                                       , 0.5f );
+
+    public static Recipe missileboatLevel3 = new Recipe( R.drawable.missileboat
+                                                       , 3
+                                                       , 15f
+                                                       , 65f
+                                                       , 144f
+                                                       , 0f
+                                                       , new Point(3, 5)
+                                                       , new PointF(2f, 0.2f)
+                                                       , new Color(1f, 0.5f, 0f)
+                                                       , new Color(0.7f, 0.05f, 0f)
+                                                       , 20f
+                                                       , 20f
+                                                       , 100
+                                                       , -20f
+                                                       , -40f );
+
+    public static Recipe sailfishLevel1 = new Recipe( R.drawable.sailfish
+                                                    , 1
+                                                    , 93f
+                                                    , 127f
+                                                    , 23
+                                                    , 0f
+                                                    , new Point(1, 1)
+                                                    , new PointF(1f, 0.2f)
+                                                    , new Color(1f, 0.3f, 0f)
+                                                    , new Color(0.7f, 0.05f, 0f)
+                                                    , 10f
+                                                    , 10f
+                                                    , 100
+                                                    , -10f
+                                                    , -20f );
+
+    public static Recipe sailfishLevel2 = new Recipe( R.drawable.sailfish
+                                                    , 2
+                                                    ,80f
+                                                    , 126f
+                                                    , 58f
+                                                    , 0f
+                                                    , new Point(1, 2)
+                                                    , new PointF(1f, 0.2f)
+                                                    , new Color(1f, 0.5f, 0f)
+                                                    , new Color(0.7f, 0.05f, 0f)
+                                                    , 30f
+                                                    , 25f
+                                                    , 100
+                                                    , -10f
+                                                    , -20f
+                                                    , 0.5f );
+
+    public static Recipe sailfishLevel3 = new Recipe( R.drawable.sailfish
+                                                    , 3
+                                                    , 52f
+                                                    , 131f
+                                                    , 100f
+                                                    , 0f
+                                                    , new Point(3, 5)
+                                                    , new PointF(2f, 0.2f)
+                                                    , new Color(1f, 0.5f, 0f)
+                                                    , new Color(0.7f, 0.05f, 0f)
+                                                    , 20f
+                                                    , 20f
+                                                    , 100
+                                                    , -20f
+                                                    , -40f );
+
+    public static Recipe battleshipLevel1 = new Recipe( R.drawable.battleship
+                                                      , 1
+                                                      , 93f
+                                                      , 120f
+                                                      , 23
+                                                      , 0f
+                                                      , new Point(1, 1)
+                                                      , new PointF(1f, 0.2f)
+                                                      , new Color(1f, 0.3f, 0f)
+                                                      , new Color(0.7f, 0.05f, 0f)
+                                                      , 10f
+                                                      , 10f
+                                                      , 100
+                                                      , -10f
+                                                      , -20f );
+
+    public static Recipe battleshipLevel2 = new Recipe( R.drawable.battleship
+                                                      , 2
+                                                      , 55f
+                                                      , 132f
+                                                      , 100f
+                                                      , 0f
+                                                      , new Point(1, 2)
+                                                      , new PointF(1f, 0.2f)
+                                                      , new Color(1f, 0.5f, 0f)
+                                                      , new Color(0.7f, 0.05f, 0f)
+                                                      , 30f
+                                                      , 25f
+                                                      , 100
+                                                      , -10f
+                                                      , -20f
+                                                      , 0.5f );
+
+    public static Recipe battleshipLevel3 = new Recipe( R.drawable.battleship
+                                                      , 3
+                                                      , 18f
+                                                      , 132f
+                                                      , 146f
+                                                      , 0f
+                                                      , new Point(3, 5)
+                                                      , new PointF(2f, 0.2f)
+                                                      , new Color(1f, 0.5f, 0f)
+                                                      , new Color(0.7f, 0.05f, 0f)
+                                                      , 20f
+                                                      , 20f
+                                                      , 100
+                                                      , -20f
+                                                      , -40f );
+
+    public static Recipe find(int typeId, int level){
+        if(typeId == battleshipLevel1.typeId && level == battleshipLevel1.level){
+            return battleshipLevel1;
+        } else if (typeId == battleshipLevel2.typeId && level == battleshipLevel2.level) {
+            return battleshipLevel2;
+        } else if (typeId == battleshipLevel3.typeId && level == battleshipLevel3.level) {
+            return battleshipLevel3;
+        }else if (typeId == sailfishLevel1.typeId && level == sailfishLevel1.level) {
+            return sailfishLevel1;
+        } else if (typeId == sailfishLevel2.typeId && level == sailfishLevel2.level) {
+            return sailfishLevel2;
+        } else if (typeId == sailfishLevel3.typeId && level == sailfishLevel3.level) {
+            return sailfishLevel3;
+        } else if (typeId == missileboatLevel1.typeId && level == missileboatLevel1.level) {
+            return missileboatLevel1;
+        } else if (typeId == missileboatLevel2.typeId && level == missileboatLevel2.level) {
+            return missileboatLevel2;
+        } else if (typeId == missileboatLevel3.typeId && level == missileboatLevel3.level) {
+            return missileboatLevel3;
+        } else {
+            return sailfishLevel1;
+        }
+    }
+}
